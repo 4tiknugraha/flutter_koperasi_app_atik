5e2ff043912f228f077fbc46d77fbde210cd66b1
4tiknugraha <atiknugrah4@gmail.com>
2024-03-13 09:14:05 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_koperasi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_koperasi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,11 @@
+// class Variables {
+//   static const String appName = 'Koperasi App';
+//   static const String baseUrl = 'http://http://10.88.10.134:8000/api/login';
+//   static const String apiVersion = 'v1';
+// }
+
+class Variables {
+  static const String appName = 'Resto App';
+  static const String apiVersion = 'v1';
+  static const String baseUrl = 'https://koperasi.komanja.my.id';
+}
